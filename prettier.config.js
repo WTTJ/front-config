ee5fad3bb2ba2baa0b8c00ad46077d2e42a5f94c
Mickaël Le Ralec <mickael.leralec@gmail.com>
2022-03-24 11:58:40 +0100

    feat: add stylelint & update README

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,1 @@
-module.exports = {
-  printWidth: 100,
-  useTabs: false,
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  arrowParens: 'avoid',
-  requirePragma: false,
-  insertPragma: false,
-  proseWrap: 'never',
-}
+module.exports = require('./lib/prettier')
