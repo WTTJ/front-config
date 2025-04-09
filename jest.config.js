b54a38df5b284621fb330c55cb09427ef8890cb7
Paul-Xavier Ceccaldi <pix@wttj.co>
2025-04-09 17:28:53 +0200

    chore: replace jest with vitest and adapt test code
    Signed-off-by: Paul-Xavier Ceccaldi <pix@wttj.co>

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    moduleFileExtensions: ["js", "mjs"],
-    testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)", "**/?(*.)+(spec|test).mjs"],
-    verbose: true,
-    transform: {
-      '^.+\\.(js|mjs)$': 'babel-jest',
-    },
-  };
